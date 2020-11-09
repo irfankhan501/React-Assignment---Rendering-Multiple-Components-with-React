3bdc8df3a5dffebda3ccd384f9d84f93f51e2233
Irfan Khan <irfankhan501@outlook.com>
2020-11-09 14:41:18 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import "../styles/App.css";
 const App = () => {
   return (
     <>
-      <h1 data-ns-text="project-name">IMDB For Books</h1>
+      <h1 data-ns-test="project-name">IMDB For Books</h1>
       <p data-ns-test="project-description">
         Here we review books and give them ratings like movies on imdb and will
         provide all the info about books.
